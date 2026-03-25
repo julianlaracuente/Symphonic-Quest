0eb311aa9fab0c038d45a4d8ae03c298de90dd1f
Julian_Laracuente <julian.laracuente@upr.edu>
2026-03-25 18:30:53 -0400

    ui improvement

--- scripts/battle_manager.gd
+++ scripts/battle_manager.gd
@@ -1,8 +1,6 @@
-extends Node
+extends Node3D
 
-
-var has_won = true
-
+@export var items : Array[Item] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
-func setup_items():
 	pass
